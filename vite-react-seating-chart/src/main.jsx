89b419e75737d2a539937e02e6e3cf54746b4302
raymondcyber <78058543+raymondcyber@users.noreply.github.com>
2025-05-30 00:21:05 +0800

    Add files via upload

---
+++ vite-react-seating-chart/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SeatingChart from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SeatingChart />
+  </React.StrictMode>,
+)
